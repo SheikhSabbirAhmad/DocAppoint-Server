b6b720a9b596fe20958535daaaf904c05826b080
Sheikh Sabbir Ahmad <22234103362@cse.bubt.edu.bd>
2026-05-21 02:30:08 +0600

    Initialize Doc-Appoint server

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId,
+} = require("mongodb");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("docAppoint");
+    const bookingCollection = db.collection("bookings");
+
+
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
