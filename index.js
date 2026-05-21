b727cfef42f9cfeac98ca5e0c48460e9d099cbf7
Sheikh Sabbir Ahmad <22234103362@cse.bubt.edu.bd>
2026-05-21 16:00:22 +0600

    Completed to add JWT-based authentication system

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
   ServerApiVersion,
   ObjectId,
 } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
 dotenv.config();
 
@@ -26,6 +27,27 @@ const client = new MongoClient(uri, {
   },
 });
 
+const JWKS = createRemoteJWKSet(new URL("http://localhost:3000/api/auth/jwks"));
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req?.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    console.log(payload);
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+};
+
 async function run() {
   try {
     await client.connect();
@@ -35,13 +57,13 @@ async function run() {
 
 
     // GET
-    app.get("/booking", async (req, res) => {
+    app.get("/booking", verifyToken, async (req, res) => {
       const result = await bookingCollection.find().toArray();
       res.send(result);
     });
 
     // POST
-    app.post("/booking", async (req, res) => {
+    app.post("/booking", verifyToken, async (req, res) => {
       const bookingData = req.body;
 
       const result = await bookingCollection.insertOne(
@@ -52,7 +74,7 @@ async function run() {
     });
 
     // PATCH
-    app.patch("/booking/:id", async (req, res) => {
+    app.patch("/booking/:id", verifyToken, async (req, res) => {
       try {
         const id = req.params.id;
 
@@ -77,7 +99,7 @@ async function run() {
     });
 
     // DELETE
-    app.delete("/booking/:id", async (req, res) => {
+    app.delete("/booking/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
 
       const result = await bookingCollection.deleteOne({
